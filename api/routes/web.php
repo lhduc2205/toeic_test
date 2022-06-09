b59ee2613cc722343d495c90c4abf73628a364ec
NguyenLamHoangAnh <just4codee@gmail.com>
2022-06-09 17:52:10 +0700

    done sidebar

--- api/routes/web.php
+++ api/routes/web.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('admin\login');
 });
+
+Route::get('/admin', function () {
+    return view('admin\index');
+});
